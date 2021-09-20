e6c9c20c9ad5a0244fe5241c0203a69da4187d0e
Jonathan <jfv213@gmail.com>
2021-09-20 13:23:47 -0400

    Basic setup and custom import visualization

--- index.ts
+++ index.ts
@@ -1,23 +1,37 @@
-import { Engine, Scene, ArcRotateCamera, HemisphericLight, Vector3, MeshBuilder, Mesh } from "babylonjs";
+import 'babylonjs-loaders'
+import { Engine, Scene, ArcRotateCamera, HemisphericLight, Vector3, MeshBuilder, Mesh, SceneLoader} from "babylonjs";
+
 
 var canvas: any = document.getElementById("renderCanvas");
 var engine: Engine = new Engine(canvas, true);
 
 function createScene(): Scene {
-  var scene: Scene = new Scene(engine);
+  const scene: Scene = new Scene(engine);
 
-  var camera: ArcRotateCamera = new ArcRotateCamera("Camera", Math.PI / 2, Math.PI / 2, 2, Vector3.Zero(), scene);
+  const camera: ArcRotateCamera = new ArcRotateCamera("camera", -Math.PI / 2, Math.PI / 2.5, 3, new Vector3(0,0,0), scene);
   camera.attachControl(canvas, true);
 
-  var light1: HemisphericLight = new HemisphericLight("light1", new Vector3(1, 1, 0), scene);
+  const light = new HemisphericLight("light", new Vector3(0, 1, 0), scene);
 
-  var sphere: Mesh = MeshBuilder.CreateSphere("sphere", { diameter: 1 }, scene);
+  // const sphere: Mesh = MeshBuilder.CreateSphere("sphere", { diameter: 2 }, scene);
+
+  // const box = MeshBuilder.CreateBox("box", {}, scene);
+  // box.position = new Vector3(1,1,1)
+  //const music = new BABYLON.Sound("cello", "sounds/cellolong.wav", scene, null, { loop: true, autoplay: true });
+  SceneLoader.ImportMesh("hm", "./models/", "Bobbin.stl", scene)
 
   return scene;
 }
+
+
 
 var scene: Scene = createScene();
 
 engine.runRenderLoop(() => {
   scene.render();
+});
+
+// Watch for browser/canvas resize events
+window.addEventListener("resize", function () {
+  engine.resize();
 });
--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
     path: path.resolve(__dirname, 'dist')
   },
   resolve: {
-    extensions: [".ts"]
+    extensions: [".ts", ".js"]
   },
   module: {
     rules: [
